05af262720f279a73c4d368c2bca293956b6e6f3
Jovan Dosen <jovandosen1994@gmail.com>
2023-02-15 11:46:32 +0100

    Moving star ship logic added

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-console.log('well and good');
+document.addEventListener("keydown", checkKeyPressed);
+
+var starShip = document.getElementById("star-ship");
+
+var starShipRightPosition = 0;
+var starShipTopPosition = 0;
+
+function checkKeyPressed(e) {
+    if(e.keyCode === 37) {
+        moveStarShipLeft();
+    }
+    if(e.keyCode === 38) {
+        moveStarShipUp();
+    }
+    if(e.keyCode === 39) {
+        moveStarShipRight();
+    }
+    if(e.keyCode === 40) {
+        moveStarShipDown();
+    }
+}
+
+function moveStarShipRight() {
+    if(starShipRightPosition === (window.innerWidth - 100)) {
+        return;
+    }
+    starShipRightPosition += 10;
+    starShip.style.left = starShipRightPosition + "px";
+}
+
+function moveStarShipLeft() {
+    if(starShipRightPosition === 0) {
+        return;
+    }
+    starShipRightPosition -= 10;
+    starShip.style.left = starShipRightPosition + "px"; 
+}
+
+function moveStarShipDown() {
+    if(starShipTopPosition === (window.innerHeight - 100)) {
+        return;
+    }
+    starShipTopPosition += 10;
+    starShip.style.top = starShipTopPosition + "px";
+}
+
+function moveStarShipUp() {
+    if(starShipTopPosition === 0) {
+        return;
+    }
+    starShipTopPosition -= 10;
+    starShip.style.top = starShipTopPosition + "px";
+}
